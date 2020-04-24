9c44c601b535e8fb213fa05ce82520773094ac4b
psialedev <psialedev@gmail.com>
2020-04-24 12:31:48 -0500

    created the edit and update controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,12 +5,20 @@ class UsersController < ApplicationController
   end
 
   def create
-   @user = User.new(
-    username: params[:user][:username],
-    email: params[:user][:email],
-    password: params[:user][:password]
-   )
+   @user = User.new(user_params)
     if @user.save 
+      redirect_to new_user_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
       redirect_to new_user_path
     else
       render :new
@@ -19,5 +27,6 @@ class UsersController < ApplicationController
 
     def user_params
       params.require(:user).permit(:username, :email, :password)
+      
     end
 end
